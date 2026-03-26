d0ce8bb490e210e19fb032ab554c33f152f2db63
lucky <salawulucky08071@gmail.com>
2026-03-26 04:16:52 +0100

    4

--- obj/Debug/net8.0/onboardingAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/onboardingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("onboardingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026ec47a3de3fd31debc5e3898d7ea1b031eb871")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa6c0fb33fd777a5c9b7a59bee2024e1660f7161")]
 [assembly: System.Reflection.AssemblyProductAttribute("onboardingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("onboardingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
